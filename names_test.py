8df640e029d3b8eaf8ea6eeb587270cad44494cd
arcctgx <arcctgx@o2.pl>
2022-06-11 01:24:43 +0200

    support double prime symbol
    Support normalization of double prime symbol (″, \u2033), often used as
    a symbol of an inch (e.g.: 12″ Single).
    
    Add unit tests.

---
+++ names_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Tests of names module."""
+
+import unittest
+from names import asciify
+
+
+class TestAsciify(unittest.TestCase):
+    def test_double_prime(self):
+        self.assertEqual(asciify('12″ version'), '12" version')
+
+
+if __name__ == '__main__':
+    unittest.main()
