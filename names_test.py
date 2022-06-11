f1c49b91ddc7b4e60f53cfb0d9b4c9ab2a6c4f1f
arcctgx <arcctgx@o2.pl>
2022-06-11 20:31:16 +0200

    refactor unit tests
    Make the test table-driven. Combine the two tests of asciify() into one,
    and add a separate test for is_typographic().
    
    Test detection and normalization of typographic apostrophe and ellipsis.

--- names_test.py
+++ names_test.py
@@ -3,16 +3,30 @@
 """Tests of names module."""
 
 import unittest
-from names import asciify
+from names import asciify, is_typographic
 
 
-class TestAsciify(unittest.TestCase):
+class TestTypographyNormalization(unittest.TestCase):
 
-    def test_typographic_hyphen(self):
-        self.assertEqual(asciify('Half‐Light'), 'Half-Light')
+    test_data = [
+        # curly apostrophe, \u2019 and ellipsis, \u2026
+        ('Could’ve Moved Mountains…', 'Could\'ve Moved Mountains...'),
+        # hyphen, \u2010
+        ('Half‐Light', 'Half-Light'),
+        # double prime or inch symbol, \u2033
+        ('12″ version', '12" version')
+    ]
 
-    def test_double_prime(self):
-        self.assertEqual(asciify('12″ version'), '12" version')
+    def test_typography_detection(self):
+        for typographic, plain_ascii in self.test_data:
+            self.assertTrue(is_typographic(typographic))
+            self.assertFalse(is_typographic(plain_ascii))
+
+    def test_asciify(self):
+        for typographic, asciified in self.test_data:
+            test_asciified = asciify(typographic)
+            self.assertEqual(asciified, test_asciified)
+            self.assertFalse(is_typographic(test_asciified))
 
 
 if __name__ == '__main__':
