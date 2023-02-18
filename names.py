5ee077dbeb11478b649a3b53aadfce9f835d03e5
arcctgx <arcctgx@o2.pl>
2023-02-18 23:34:04 +0100

    provide better digit substitution strings
    10-digit pseudo-random strings turned out to be insufficient in
    practice. Track titles like "Mr. Eddy's Theme 1" were not matched
    correctly when fuzzy matching was enabled. It appears that providing
    better digit substitution strings is necessary.
    
    Previously used strings have Jaro-Winkler similarity of about 50%, which
    is pretty high and does not sufficiently increase edit distance between
    titles like "Song, Part X". Jaro-Winkler similarity of the new strings
    is exactly 0%, which coupled with their length sufficiently increases
    the edit distance and improves the odds of finding the correct match.
    
    Reformat names.py with YAPF.

--- names.py
+++ names.py
@@ -5,7 +5,6 @@ understood as names or titles.
 
 import jellyfish
 
-
 SINGLE_QUOTES = '\u2018\u2019\u201a\u201b'
 DOUBLE_QUOTES = '\u201c\u201d\u201e\u201f\u2033'
 DASHES = '\u2010\u2012\u2013\u2014\u2015'
@@ -32,7 +31,7 @@ def asciify(name):
         return name
 
     typographic = SINGLE_QUOTES + DOUBLE_QUOTES + DASHES
-    plain_ascii = '\''*len(SINGLE_QUOTES) + '"'*len(DOUBLE_QUOTES) + '-'*len(DASHES)
+    plain_ascii = '\'' * len(SINGLE_QUOTES) + '"' * len(DOUBLE_QUOTES) + '-' * len(DASHES)
     mapper = str.maketrans(typographic, plain_ascii)
 
     return name.translate(mapper).replace('\u2026', '...')
@@ -72,14 +71,24 @@ def is_typographic(name):
 def replace_numbers(name):
     """
     Return a new string where all embedded numbers are replaced with
-    fixed pseudo-random strings. The purpose of this is to increase
+    fixed long sequences of letters which differ from one another in
+    terms of fuzzy string matching. The purpose of this is to increase
     the edit distance between track titles that differ only by numbers,
     such as "Song Title, Part N".
     TODO: handle Roman numerals
     """
-    replace = { '0': 'iuxhxrugig', '1': 'odoiqlazbk', '2': 'kuqhzswjlz', '3': 'maoutyqrob',
-            '4': 'kjhlzjijzh', '5': 'yiqfvoncaw', '6': 'xamlrqeigm', '7': 'xcuijkfqmp',
-            '8': 'zohysugnvh', '9': 'qoamtosahy' }
+    replace = {
+        '0': 'a' * 48,
+        '1': 'b' * 48,
+        '2': 'c' * 48,
+        '3': 'd' * 48,
+        '4': 'e' * 48,
+        '5': 'f' * 48,
+        '6': 'g' * 48,
+        '7': 'h' * 48,
+        '8': 'i' * 48,
+        '9': 'j' * 48
+    }
 
     for key, val in replace.items():
         name = name.replace(key, val)
@@ -90,7 +99,7 @@ def cmp_fuzzy_normalized_numbers(name, other_name):
     """
     Compare two strings using fuzzy algorithm. Both input strings are
     converted to lower case, typographic characters are changed to ASCII
-    counterparts, and numbers are replaced with pseudo-random strings to
+    counterparts, and numbers are replaced with fixed letter sequences to
     increase edit distance between strings which only differ by digits.
     """
     cmp_name = replace_numbers(normalize(name))
