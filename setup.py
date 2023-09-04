2cc4f97f00f117f4d46f477bc2061694d62625d1
arcctgx <arcctgx@o2.pl>
2023-09-04 13:32:32 +0200

    v1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('LICENSE', encoding='utf-8') as f:
     license_ = f.read()
 
 setup(name='mbnames',
-      version='0.1.0',
+      version='v1.0.0',
       description=
       'Utility functions for working with MusicBrainz or Last.fm artist credit and title strings.',
       long_description=readme,
